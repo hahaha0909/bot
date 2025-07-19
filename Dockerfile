fcd9f8f94a99e46223fbfed3edc3cba69b70a7ae
Федор <fedorgym2005@gmail.com>
2025-07-19 15:42:02 +0700

    Revert "Added start in Docker"
    This reverts commit 896ec65d2cd4c3ce165726c35ee21ab8ead6986d, reversing
    changes made to 1afa8ce49aeba74cc95e51af42ecc0d26750acea.

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM gcc:12.2.0
-
-WORKDIR /app
-COPY main.cpp .
-
-RUN g++ -o start main.cpp
-
-CMD ["./start"]
--- main.cpp
+++ main.cpp
@@ -1,12 +1,6 @@
 #include "iostream"
-using namespace std;
 int main() {
-    int i=0;
-    while (i < 10) {
-        cout << "Hello world" << "\n";
-        i++;
-    }
-
+    std::cout << 'a';
     return 0;
 }
 
